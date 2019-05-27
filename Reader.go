6b776a8c5f42a5ed10b88e27da78f2909d77e3b9
Eduard Urbach <e.urbach@gmail.com>
2019-05-27 09:24:21 +0900

    Store pointers in the buffer pool

--- Reader.go
+++ Reader.go
@@ -8,21 +8,22 @@ import (
 // bufferPool is a pool for byte buffers.
 var bufferPool = sync.Pool{
 	New: func() interface{} {
-		return make([]byte, 4096)
+		buffer := make([]byte, 4096)
+		return &buffer
 	},
 }
 
 // Reader hashes all the bytes in the given reader.
 func Reader(reader io.Reader) uint64 {
 	var x uint64
-	buffer := bufferPool.Get().([]byte)
+	buffer := *bufferPool.Get().(*[]byte)
 
 	for {
 		n, err := reader.Read(buffer)
 		x = add(x, buffer[:n])
 
 		if err != nil {
-			bufferPool.Put(buffer)
+			bufferPool.Put(&buffer)
 			return x
 		}
 	}
